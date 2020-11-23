487f2b167ade452acd27882437a85e74f7810049
yuriy-bar <urkourkovich@gmail.com>
2020-11-23 13:57:29 +0000

    Gallery code

---
+++ js/carousel.js
@@ -0,0 +1,37 @@
+const images = [
+    'img/gallery1.jpg',
+    // 'img/gallery2.jpg',
+    'img/gallery3.jpg',
+    'img/gallery4.jpg',
+]
+
+let currentIdx = 0;
+
+function slide() {
+    currentIdx = ++currentIdx > images.length - 1 ? 0 : currentIdx;
+    showSlide(currentIdx);
+}
+
+setInterval(slide, 100000);
+
+function back() {
+    currentIdx = --currentIdx < 0 ? images.length - 1 : currentIdx;
+    showSlide(currentIdx);
+}
+
+document.querySelector('.carousel .btn-back').addEventListener('click', back);
+
+function showSlide(idx) {
+    document.querySelector('.carousel img').src = images[idx];
+}
+
+function fwd() {
+    currentIdx = ++currentIdx > images.length - 1 ? 0 : currentIdx;
+    showSlide(currentIdx);
+}
+
+document.querySelector('.carousel .btn-fwd').addEventListener('click', fwd);
+function showSlide(idx) {
+    document.querySelector('.carousel img').src = images[idx];
+}
+
