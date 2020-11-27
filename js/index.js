21967a7368b5154c76091efef84f72d085b4fe96
Urko <urkourkovich@gmail.com>
2020-11-27 17:38:39 +0200

    Bag fixed

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-// const clock = document.querySelector('.clock');
-// setInterval( () => clock.innerText = new Date().toLocaleTimeString(), 1000 )
+const clock = document.querySelector('.clock');
+setInterval( () => clock.innerText = new Date().toLocaleTimeString(), 1000 )
 
-// const clock = document.querySelector('.clock');
-// setInterval( () => clock.innerText = new Date().toLocaleTimeString(), 1000  )
+const clock = document.querySelector('.clock');
+setInterval( () => clock.innerText = new Date().toLocaleTimeString(), 1000  )
